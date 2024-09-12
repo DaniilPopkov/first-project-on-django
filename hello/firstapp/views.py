898906e8552429cae7fddf1a534d44940a92a9de
Your Name <you@example.com>
2024-09-12 12:57:34 +0300

    12.09 Попков Якушева Внешний дизай,добавил папки css and images

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -41,13 +41,13 @@ def details(request):
     return HttpResponsePermanentRedirect("/")
 
 def index(request):
-    # return render(request, "firstapp/home.html")
-    header = "Персональные данные" # обычная переменная
-    langs = ["Английский", "Немецкий", "Испанский"] # массив
-    user = {"name": "Максим,", "age": 30} # словарь
-    addr = ("Виноградная", 23, 45) # кортеж
-    data = {"header": header, "langs": langs, "user": user, "address": addr}
-    return render(request, "index.html", context=data)
+    return render(request, "firstapp/home.html")
+    # header = "Персональные данные" # обычная переменная
+    # langs = ["Английский", "Немецкий", "Испанский"] # массив
+    # user = {"name": "Максим,", "age": 30} # словарь
+    # addr = ("Виноградная", 23, 45) # кортеж
+    # data = {"header": header, "langs": langs, "user": user, "address": addr}
+    # return render(request, "index.html", context=data)
 
 
 
