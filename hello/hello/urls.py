aebcb9548f68899e86a409f19a1dffb70cc1f9d8
Your Name <you@example.com>
2024-09-12 11:11:22 +0300

    12.09 Попков и Якушева Переадресация по сайту

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -16,14 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from firstapp import views
 
 
 urlpatterns = [
-    path("", views.index),
-    path ( 'about' , views. about),
-    path ( 'contact', views. contact),
-]
+    path('', views.index),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
+    path('products/<int:productid>/', views.products),
+    path('users/', views.users),
+     path('about/', views.about),
+    path('contact/', views.contact),
+    path('details/', views.details),
+] 
 
 
 
