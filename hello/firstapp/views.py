ba82ad7bfa824326317eb3e7f211eba75c336411
DaniilPopkov <daniilpopkov66@gmail.com>
2024-09-24 16:17:28 +0300

    24.09 Попков и Якушева, работа с тегами для даты и времени, условий, перебора из массива, с заданной переменной.

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,53 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.template.response import TemplateResponse
+from django.http import *
 
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World! Это мой первый проект на Django!")
+            cat = ["Ноутбуки", "Принтеры", "Сканеры", "Диски", "Шнуры"]
+            return render(request, "firstapp/index.html", context={"cat": cat})
 
-def index(request):
-    return HttpResponse("<h2>Глaвнaя</h2>")
-
-def about(request):
-    return HttpResponse("<h2>0 сайте</h2>")
-
-def contact(request):
-    return HttpResponse("<h2>Koнтaкты</h2>")
-
-def products(request, productid):
-    category = request.GET.get("cat", "")
-    output = "<h2>Продукт № {0} Категория: {1}</h2>" .format(productid, category)
-    return HttpResponse(output)
-
-def users(request):
-    id = request.GET.get("id", 1)
-    name = request.GET.get("name", "Максим")
-    output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3 >" .format(id, name)
-    return HttpResponse(output)
-def index(request):
-    return HttpResponse("Index")
-
-def about(request):
-    return HttpResponse("About")
-
-def contact(request):
-    return HttpResponseRedirect("/about")
-
-def details(request):
-    return HttpResponsePermanentRedirect("/")
-
-def index(request):
-    return render(request, "firstapp/index.html")
-    # header = "Персональные данные" # обычная переменная
-    # langs = ["Английский", "Немецкий", "Испанский"] # массив
-    # user = {"name": "Максим,", "age": 30} # словарь
-    # addr = ("Виноградная", 23, 45) # кортеж
-    # data = {"header": header, "langs": langs, "user": user, "address": addr}
-    # return render(request, "index.html", context=data)
+        # header = "Персональные данные" # обычная переменная
+        # langs = ["Английский", "Немецкий", "Испанский"] # массив
+        # user = {"name": "Максим,", "age": 30} # словарь
+        # addr = ("Виноградная", 23, 45) # кортеж
+        # data = {"header": header, "langs": langs, "user": user, "address": addr}
+        # return render(request, "index.html", context=data)
 
 
 
