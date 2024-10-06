1bad4af4bdbf1f395e2f762d6ef1c7cb9b26254e
DaniilPopkov <popkovdaniil66@gmail.com>
2024-10-06 14:34:27 +0300

    06.10 Якушева и Попков, работы с объектами модели данных: редактирование и удаление информации из БД, организация связей между таблицами

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,12 +1,46 @@
-from django.http import *
-from .forms import UserForm
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Person
+from .models import Product
+from .models import Company
 
+# получение данных из БД и загрузка index.html
 def index(request):
-    userform = UserForm()
-    if request.method == "POST":
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = userform.cleaned_data["name"]
-            return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
-    return render(request, "firstapp/index.html", {"form": userform})
+ people = Person.objects.all()
+ return render(request, "index.html", {"people": people})
+
+# сохранение данных в БД
+def create(request):
+ if request.method == "POST":
+    klient = Person()
+    klient.name = request.POST.get("name")
+    klient.age = request.POST.get("age")
+    klient.save()
+ return HttpResponseRedirect("/")
+
+# изменение данных в БД
+def edit(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        if request.method == "POST":
+            person.name = request.POST.get("name")
+            person.age = request.POST.get("age")
+            person.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit.html", {"person": person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+# удаление данных из БД
+def delete(request, id):
+ try:
+    person = Person.objects.get(id=id)
+    person.delete()
+    return HttpResponseRedirect("/")
+ except Person.DoesNotExist:
+    return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+
+firma = Company.objects.create(name=" Электрон")
+firma.product_set.create(name="Samsung S20", price=42000)
+ipad = Product(name="iPad", price=34200)
+firma.product_set.add(ipad, bulk=False)
